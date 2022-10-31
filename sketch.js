6f10f3e25a2fa190b928bcb41ea6ffadef3e9f7e
beatriceulivi <beatrice.ulivi@mail.polimi.it>
2022-10-31 13:31:49 +0100

    Animated cover

--- sketch.js
+++ sketch.js
@@ -1,16 +1,63 @@
+let myImage;
+let song;
+let amp;
+let button;
+
+let volhistory = [];
+
+//load song + image
 function preload() {
-	// put preload code here
+  song = loadSound("./assets/Always Then.mp3");
+  myImage = loadImage("./assets/background.png")
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(700, 700);
+  angleMode(DEGREES);
+
+  //button start&stop
+  button = createButton("PLAY");
+  button.mousePressed(playSong);
+  button.position(310, 620);
+	button.size(80,30);
+	button.style("background-color",color(150));
+	button.style("font-size","14px");
+	button.style("font-family",'Space Mono');
+
+  song.play();
+  amp = new p5.Amplitude();
 }
 
 function draw() {
-	// put drawing code here
+  background(0);
+  image(myImage,0,0,myImage.widht, myImage.height);
+
+  let vol = amp.getLevel();
+  volhistory.push(vol);
+  stroke(0);
+  noFill();
+
+  //create graph using beginShape function
+  translate(width / 2, height / 2);
+  beginShape();
+  for (let i = 0; i < 360; i++) {
+    let r = map(volhistory[i], 0, 1, 10, 700);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
+  }
+  endShape();
+
+  //the graph clears when it reaches the end
+  if (volhistory.length > 360) {
+    volhistory.splice(0, 1);
+  }
+}
+
+function playSong() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
 }
